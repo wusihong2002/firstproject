09f9c891474a50b6a88b3817e87e2082a20d26a0
linhong <w3419927307@163.com>
2023-10-03 11:58:01 +0800

    good

---
+++ PythonApplication2/PythonApplication2.py
@@ -0,0 +1,82 @@
+#x="hello python interpreter!"
+#print(x)
+#x="hello "
+##print(x)
+##print(name.upper())#全大写
+##print(name.lower())#全小写
+##first_name="ada"
+##last_name="lovelace"
+#full_name=f"{first_name} {last_name}"
+#print(f"Hello,{full_name.upper()}")
+#print("python")
+#print("\tpython")
+#print("langguages:\nc\nc++\njava\npython")
+#print("langguages:\n\tc\n\tc++\n\tjava\n\tpython")
+#favorite_language='python '
+#favorite_language
+#favorite_language=favorite_language.rstrip()
+#favorite_language
+#x,y,z=0,0,0
+#print(x,y,z)
+#MAX_CONNECTIONS=5000
+#print(MAX_CONNECTIONS)
+#bicycles= ['tiek','cannondale','redline','specialized']
+#print(bicycles)
+#print(bicycles[0])
+#print(bicycles[0].title())#可以用字符串方法
+#print(bicycles[-1])#倒数第一个元素
+#motorcycles=['haoda','yamaha','suzuki']
+#print(motorcycles)
+#motorcycles[0]='dacati'
+#print(motorcycles)
+#motorcycles.append('ducati')#添加在末尾
+#print(motorcycles)
+#motorcycles=[]
+#motorcycles.append('haoda')
+#motorcycles.append('yamaha')
+#motorcycles.append('suzuki')
+#print(motorcycles)
+#motorcycles.insert(0,'ducati')#insert可以在任何位置插入元素，需指定新元素的索引
+#print(motorcycles)
+#motorcycles=['haoda','yamaha','suzuki','dacati']
+#too_expensive='dacati'
+#motorcycles.remove(too_expensive)
+#del motorcycles[0]
+#print(motorcycles)
+#print(f"\nA {too_expensive.title()} is too expensive for me.")
+#cars=['bmw','audi','toyota','subaru']
+#cars.sort(reverse=True)
+#cars.sort()
+#print("Here is the original list:")
+#print(cars)
+#print("\nHere is the sorted list:")
+#print(sorted(cars))
+#print("\nHere is the original list again:")
+#print(cars)
+#len(cars)
+#num=len(cars)
+#print(num)
+#print(cars)
+#motorcycles=['haoda','yamaha','suzuki','dacati']
+#for person in motorcycles:
+   # print(person)
+    #print(f"{person.title()}, that was a great trick")
+#print("think you,everyone.")
+#for value in range(1,5):
+  #  print(value)
+#numbers = list(range(1,6))
+#print(numbers)
+#even_number=list(range(2,11,2))
+#print(even_number)
+#squares=[]
+#for value in range(1,11):
+#    squares.append(value**2)
+#print(squares)
+#digits=[1,2,3,4,5,6,7,8,9,0]
+#min(digits)
+#squares=[value**2 for value in range(1,11)]
+#print(squares)
+for value in range(3,30,3):
+    values=value/3
+    print(values)
+
