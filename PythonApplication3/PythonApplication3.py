4f197962d291842f68490cbed3ff8c767b6034d0
linhong <w3419927307@163.com>
2023-10-04 09:52:56 +0800

    good

---
+++ PythonApplication3/PythonApplication3.py
@@ -0,0 +1,19 @@
+#players=['charles','martina','michael','florence','eli']
+#for player in (players[:3]):
+#    print(player.title())
+#my_foods=['pizza','falafel','carrot cake']
+#friend_foods=my_foods[:]
+#my_foods.append('ice cream')
+#friend_foods.append('cannoli')
+#print(my_foods)
+#print(friend_foods)
+#dimensions = (200,50)
+#for dimenison in dimensions:
+#    print(dimenison)
+#dimensions = (400,100)
+#for dimenison in dimensions:
+#    print(dimenison)
+#foods = (1,2,3,4,5)
+#foods = (1,2,3,6,7)
+#for food in foods:
+#    print(food)
