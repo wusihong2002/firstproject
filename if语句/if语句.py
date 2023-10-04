eab289b57d360151af20956220f38259c2668d08
linhong <w3419927307@163.com>
2023-10-04 16:51:16 +0800

    good

---
+++ if语句/if语句.py
@@ -0,0 +1,89 @@
+#cars=['audi','bmw','subaru','toyota']
+#car = 'subaru'
+#if car  in cars:
+#    print("i predict true")
+#car = 'audi'
+#if car in cars:
+#    print("i predict false")
+#for car in cars:
+#    if car == 'bmw':
+#        print(car.upper())
+#    else:
+#        print(car.title())
+#banned_users = ['andrew','carolina','david']
+#user = 'marie'
+#if user not in banned_users:
+#    print(f"{user.title()}, you can post a response if you wish.")
+#age = 19
+#if age >=18:
+#    print("you are old enough to vote!")
+#else:
+#    print("sorry, you are too youung to vote")
+
+#age=12
+#if age <=4:
+#    money=0
+#elif age <18:
+#    money=25
+#else:
+#    money=40
+#print(f"you need pay ${money}")
+ 
+#alien_color=['green','yellow','red']
+#if alien_color == 'green':
+#    print("Score 5 points")
+
+#requested_toppings = ['mushrooms','green peppers','exxtra cheese']
+#for requested_topping in requested_toppings:
+#    if requested_topping == 'green peppers':
+#        print("sorry, we are out of green peppers right now.")
+#    else:
+#        print(f"Adding {requested_topping}.")
+#print("\nFinished making your pizza!")
+
+#requested_toppings = []
+#if requested_toppings:
+#    for requested_topping in requested_toppings:
+#        print(f"Adding {requested_topping}.")
+#    print(f"Adding {requested_topping}.")
+#else:
+#    print("Are you sure you want a plain pizza?")
+
+#available_toppings = ['mushrooms','olices','green peppers','pepperoni','pineapple','extra cheese']
+#requested_toppings = ['mushrooms','french fries','extra cheese']
+#for requested_topping in requested_toppings:
+#    if requested_topping in available_toppings:
+#        print(f"Adding {requested_topping}.")
+#    else:
+#        print(f"Sorry, we don't have {requested_topping}.")
+#print("\nFinished making your pizza!")
+
+#gls = ['tow','one','four','admin','steam']
+#for gl in gls:
+#    if gl == 'admin':
+#        print("Hello admin 123")
+#    else:
+#        print(f"hello {gl}")
+
+#yh = ['tow','one','four','admin','steam']
+#if yh:
+#    print("hello")
+#else:
+#    print("person")
+
+#t_uses= ['tow','one','four','admin','steam']
+#f_uses= ['tow','steam','qw','qe','qr']
+#for use in f_uses:
+#    if use in t_uses:
+#        print(use)
+#    else:
+#        print("false")
+
+nums = ['1','2','3','4','5','6','7','8','9']
+for num in nums:
+    if num == '1':
+        print(f"{num}st")
+    elif num == '2':
+        print(f"{num}nd")
+    else:
+        print(f"{num}th")
